2164d713be3439872cc032ffb5175027e4335fd1
melisbm <miquelangel.melis@gmail.com>
2026-02-24 17:42:25 +0100

    field area from difficulty Field class construct

--- src/Field.java
+++ src/Field.java
@@ -4,11 +4,23 @@ public class Field {
 
     char[][] fieldCells = new char[width][height];
 
-    public Field(int height, int width){
-        this.height = height;
-        this.width = width;
+    public Field(String difficulty){
+        if(difficulty.equals("easy")){
+            this.height = 9;
+            this.width = 9;
+        }
+        else if(difficulty.equals("medium")){
+            this.height = 16;
+            this.width = 16;
+        }
+        else{
+            this.height = 16;
+            this.width = 30;
+        }
     }
 
-
+    public String fieldToString(String difficulty){
+        return "";
+    }
 
 }
