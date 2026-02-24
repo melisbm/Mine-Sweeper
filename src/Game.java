9c0dad838481adf5e87da84b92e9ff12d35694c8
melisbm <miquelangel.melis@gmail.com>
2026-02-24 18:51:55 +0100

    startGame and gameLoop in Game class

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,13 @@
+import java.util.*;
+
 public class Game {
+
+    private void startGame(){
+        Field field = new Field("medium");
+    }
+
+    private void gameLoop(){
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
 }
